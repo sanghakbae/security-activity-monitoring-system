dc27bc86737384af04cd7adbca774c138a5c6549
sexysanghak <totoriverce@gmail.com>
2026-03-24 21:14:40 +0900

    fix: vite base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/security-activity-monitoring-system/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
