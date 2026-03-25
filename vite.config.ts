7737d48a0bf99d8a94d9d7561fb85f4fb742dc5d
sexysanghak <totoriverce@gmail.com>
2026-03-25 10:42:21 +0900

    editing vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const repoName = 'security-activity-monitoring-system'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : "/",
-});
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
